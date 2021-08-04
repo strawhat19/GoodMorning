15653f1739769a78f7bf55bbf8853f770062c439
Rakib Ahmed <rakib987@gmail.com>
2021-08-03 23:42:03 -0400

    Successfully Fetching Reddit Memes

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -2,11 +2,20 @@
 console.log('This is where we fetch our info');
 
 var redditKey = 'NWALDmX-ufQV53BKp0JLVw';
-var redditRequestURL = 'https://www.reddit.com/r/videos/new?limit=25';
+var redditRequestURL = 'https://www.reddit.com/r/memes/new.json?limit=12';
 
 fetch(redditRequestURL)
 .then(response => {
     return response.json();
 }).then(data => {
     console.log(data);
+    for (var i = 0; i < data.data.children.length; i++) {
+        console.log(data.data.children[i]);
+        console.log(data.data.children[i].data.url);
+        var reddit = $('.reddit');
+        var memes = data.data.children[i].data.url;
+        var memeTitle = data.data.children[i].data.title;
+        var memeImage = $(`<div class="redditMemeElement">${i+1 + '. '}${memeTitle}<img class="redditMeme" src="${memes}"></div>`);
+        reddit.append(memeImage);
+    }
 })
