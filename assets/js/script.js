71be90a2b4243b5af20a7f6f78fa548442d4e449
9ydaniels <yishaydaniels18@gmail.com>
2021-08-03 22:14:18 -0400

    new branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
   //hi
   var cnn = document.querySelector('#cnn');
   var reddit = document.querySelector('#reddit');
+  var abc = document.querySelector('abc');
+  var espn = document.querySelector('ESPN');
+  var cspan = document.querySelector('CNBC');
+  var news = document.querySelector('news');
