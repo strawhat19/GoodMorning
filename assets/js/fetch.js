7dccd7261137d9b8547d58fb603b0ddf5ae3f917
Rakib Ahmed <rakib987@gmail.com>
2021-08-03 21:47:59 -0400

    Changes

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,3 +1,12 @@
 // Fetch Script Code
 console.log('This is where we fetch our info');
 
+var redditKey = 'NWALDmX-ufQV53BKp0JLVw';
+var redditRequestURL = 'https://www.reddit.com/r/videos/new?limit=25';
+
+fetch(redditRequestURL)
+.then(response => {
+    return response.json();
+}).then(data => {
+    console.log(data);
+})
