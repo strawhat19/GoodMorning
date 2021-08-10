bf9f72c374f6e6851b8b01f37b57715edf64cef3
Rakib Ahmed <rakib987@gmail.com>
2021-08-10 06:54:01 -0400

    Changes to Fetch

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -94,11 +94,12 @@ fetch(nationalArchivesAPI)
 })
 
 // Fetching Nasa Images
-var nasaImages = `https://images-api.nasa.gov/search?q=planet`;
+var nasaKey = `wC0OktvMTSEtJByir363uFfuw3YhKmezYxfQ1BGq`;
+var nasaImagesSearch = `https://images-api.nasa.gov/search?q=planet`;
 var nasaImagesKey = `https://images-api.nasa.gov?api_key=wC0OktvMTSEtJByir363uFfuw3YhKmezYxfQ1BGq`;
 
 // NASA Images API
-fetch(nasaImages)
+fetch(nasaImagesSearch)
 .then(response => {
     return response.json();
 }).then(data => {
@@ -107,9 +108,44 @@ fetch(nasaImages)
     console.log(data);
     for (var i = 0; i < data.collection.items.length; i++) {
         var nasaImageTitle = data.collection.items[i].data[0].title;
-        var nasaImageElement = $(`<div class="nasaImageElement">${nasaImageTitle}</div>`);
+        var nasaImageTitles = JSON.parse(localStorage.getItem('Nasa Titles')) || [];
+        nasaImageTitles.push(nasaImageTitle);
+        nasaImageTitles.splice(100);
+        localStorage.setItem('Nasa Titles',JSON.stringify(nasaImageTitles));
+        var nasaSideFetch = data.collection.items[i].href;
+        fetch(nasaSideFetch).then(newResponse => {return newResponse.json()}).then((linkData,nasaImage) => {
+            var nasaImageArray = filterImageGallery(linkData,'.jpg');
+            var nasaLargeImage = nasaImageArray[0];
+            var nasaVideoArray = filterImageGallery(linkData,'.mp4');
+            var nasaVideo = nasaVideoArray[0];
+            var nasaImageTitles = JSON.parse(localStorage.getItem('Nasa Titles')) || [];
+            console.log(nasaImageTitles);
+            var nasaImageElement = $(`
+            <div class="nasaImageElement element">
+                <div class="nasaImageTitle elementTitle">
+                    <div class="titleRow">
+                        <span class="index">${i + '. '}</span>
+                        <a class="nasaImageLink elementLink" target="_blank" href="${nasaLargeImage}">${nasaImageTitles[i]}</a>
+                    </div>
+                    <span class="byLine"></span>
+                 </div>
+                <a class="nasaImageLink elementLink nasaImageSource" target="_blank" href="${nasaLargeImage}">
+                    <img class="nasaImage" src="${nasaLargeImage}">
+                </a>
+            </div>
+            `);
+            localStorage.setItem('Nasa Image Links',JSON.stringify(nasaImageArray));
+            localStorage.setItem('Nasa Video Links',JSON.stringify(nasaVideoArray));
+            function filterImageGallery(array, query) {
+                return array.filter(function(jpg) {
+                    return jpg.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+                })
+            }
+            nasa.append(nasaImageElement);
+            return nasaImage;
+        })
     }
-    nasa.append(nasaImageElement);
+    
 })
 
 
@@ -200,17 +236,17 @@ fetch(newsRequestURL)
     }
 })
 
-// Fetching Nasa Data
-var nasaAPI = `https://api.nasa.gov/planetary/apod?api_key=wC0OktvMTSEtJByir363uFfuw3YhKmezYxfQ1BGq`;
+// // Fetching Nasa Data
+// var nasaAPI = `https://api.nasa.gov/planetary/apod?api_key=wC0OktvMTSEtJByir363uFfuw3YhKmezYxfQ1BGq`;
 
-// NASA API
-fetch(nasaAPI)
-.then(response => {
-    return response.json();
-}).then(data => {
-    console.log('NASA Data Is:');
-    console.log(data);
-})
+// // NASA API
+// fetch(nasaAPI)
+// .then(response => {
+//     return response.json();
+// }).then(data => {
+//     console.log('NASA Data Is:');
+//     console.log(data);
+// })
 
 
 // Deprecated
